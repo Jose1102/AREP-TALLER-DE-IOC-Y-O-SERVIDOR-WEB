447f07e978c0643a470033ce5a7151187d175518
Jose1102 <jose.castroortega1102@gmail.com>
2021-09-15 00:33:20 -0500

    Modified js

--- src/main/resources/imagenes/index.js
+++ src/main/resources/imagenes/index.js
@@ -2,7 +2,7 @@ var txt;
 var r = confirm("¿ Deseas volver al menú principal ?");
 if (r == true) {
     txt = "You pressed OK!";
-    window.location.href = "http://localhost:36000/index.html"
+    window.location.href = "https://taller-ioc.herokuapp.com/index.html"
 } else {
     txt = "You pressed Cancel!";
 }
